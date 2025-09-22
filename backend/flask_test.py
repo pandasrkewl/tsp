5c46c5ce058ec1d48bc47af002c90e3e1167763e
Max Chiu <mc4487@drexel.edu>
2025-09-21 23:03:23 -0400

    Moved graph.png to frontend and edited flask_test to practice sqlalchemy

--- backend/flask_test.py
+++ backend/flask_test.py
@@ -1,6 +1,12 @@
 from flask import Flask
-import sqlalchemy
+from sqlalchemy import create_engine, text
 app = Flask(__name__)
+
+
+engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+with engine.connect() as conn:
+    result = conn.execute(text("select 'hello world'"))
+    print(result.all())
 
 @app.route('/')
 def hello_world():
